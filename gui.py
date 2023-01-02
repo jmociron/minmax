91e8e212d6d51c06e17813241b3760adc707ca83
Jamie Mari Ciron <jociron@up.edu.ph>
2023-01-02 10:41:03 +0800

    feat: checks for all winning conditions

--- gui.py
+++ gui.py
@@ -13,8 +13,8 @@ states = [
     [0, 0, 0]
 ]
 
-player = "O"
-win = False
+players = ["O", "X"]
+player = players[1]
 
 class Puzzle(Frame):
 
@@ -25,23 +25,56 @@ class Puzzle(Frame):
         self.createBoard() 
     
     def check_col(self):
-        pass        
-    
+        for i in range(len(states)):
+            col = list(map(lambda row: row[i], states))
+            if col[0] in players:
+                match = all(state == col[0] for state in col)
+                if match:
+                    print(col[0], "wins!")
+                    return True
+        return False
+        
     def check_row(self):
+
         for row in states:
-            print(row)
-            match = all(state == row[0] for state in row)
-            print(match)
-            if match:
-                return True
+            if row[0] in players:
+                match = all(state == row[0] for state in row)
+                if match:
+                    print(row[0], "wins!")
+                    return True
         return False
 
     def check_diag(self):
-        pass
+        
+        left_diag = []
+        right_diag = []
+
+        # retrieves states in both possible diagonals
+        for i in range(len(states)):
+            left_diag.append(states[i][i])
+            for j in range(len(states)):
+                if i+j == len(states)-1:
+                    right_diag.append(states[i][j])
+
+        # checks for the left diagonal
+        if left_diag[0] in players:
+            match = all(state == left_diag[0] for state in left_diag)
+            if match:
+                print(left_diag[0], "wins!")
+                return True
+
+        # checks for the right diagonal
+        elif right_diag[0] in players:
+            match = all(state == right_diag[0] for state in right_diag)
+            if match:
+                print(right_diag[0], "wins!")
+                return True
+        return False
 
     def check_win(self):
-        if self.check_col() or self.check_row():
-            print("You win!")
+        self.check_col()
+        # self.check_row()
+        # self.check_diag()
 
     def clicked(self, row, col):
         buttons[row][col].configure(text = player)
